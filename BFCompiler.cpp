e87dd0eea0923f508b53a5880623167f0d78809f
Cameron McGinley <mcginleycameron@gmail.com>
2022-06-12 17:47:50 -0400

    Store data in struct rather than instr string (slow)

--- BFCompiler.cpp
+++ BFCompiler.cpp
@@ -7,18 +7,56 @@
 #include <string>
 #include <chrono>
 #include <regex>
+#include <unordered_map>
 using namespace std::chrono;
 
-struct Cell {
-    char instruction;
-    // Counter is used as the moveto index for loop brackets
-    int counter;
+enum instructions {
+    traverse_right,
+    traverse_left,
+    increment,
+    decrement,
+    loop_open,
+    loop_close,
+    output,
+    input,
+    pattern_zero,
+    pattern_substract,
+    pattern_move,
+    dead_instr, // For temp vector set, don't move to final vector
+};
+
+instructions key_to_hash(char key) {
+    if (key == '>') return traverse_right;
+    if (key == '<') return traverse_left;
+    if (key == '+') return increment;
+    if (key == '-') return decrement;
+    if (key == '[') return loop_open;
+    if (key == ']') return loop_close;
+    if (key == '.') return output;
+    if (key == ',') return input;
+}
+
+struct Instruction {
+    instructions key;
+
+    // Depicts different variations for pattern matches
+    std::string variation;
+
+    // For condensable instructions
+    int repeat;
+
+    // For loop brackets
+    int moveto;
+
+    // Various helpful vars for patterns, hash map too slow
+    int dist;
+    int dist_to_big;
+    int dist_to_ans;
 };
 
 class Tape {
-    // Each instruction is a pair of (instruction, moveto index)
-    // the 2nd index only useful for loops
-    std::vector<Cell> final_instruction_set;
+    std::vector<Instruction> temp_instruction_set;
+    std::vector<Instruction> final_instruction_set;
     int curr_instruction_index;
 
     std::vector<char> tape;
@@ -96,24 +134,24 @@ public:
         tape[curr_tape_index] = n;
     }
 
-    void loop_open() {
+    void floop_open() {
         // Check if should enter loop (current not 0)
         if (tape[curr_tape_index] > 0) {
             if (debug) log("Loop entered");
         }
         // else, continue to after loop, find matching pair
         else {
-            curr_instruction_index = final_instruction_set[curr_instruction_index].counter;
+            curr_instruction_index = final_instruction_set[curr_instruction_index].moveto;
             curr_instruction_index--; // Avoid instruction increment in execute()
 
             if (debug) log("Loop skipped");
         }
     }
 
-    void loop_close() {
+    void floop_close() {
         // Check if should jump back (current not 0)
         if (tape[curr_tape_index] > 0) {
-            curr_instruction_index = final_instruction_set[curr_instruction_index].counter;
+            curr_instruction_index = final_instruction_set[curr_instruction_index].moveto;
             curr_instruction_index--; // Avoid instruction increment in execute()
 
             if (debug) log("Loop repeated");
@@ -130,43 +168,49 @@ public:
         // Takes in the raw instruction set
         // Remove all whitespace
         raw_instructions.erase(std::remove_if(raw_instructions.begin(), raw_instructions.end(), ::isspace), raw_instructions.end());
-
         syntax_check(raw_instructions);
 
-        // Converts the raw string into file instruction set of vector<Cell>
-        coalesce_operands(raw_instructions);
+        // Convert raw instructions to vector of Instructions
+        for (char c : raw_instructions) {
+            Instruction new_instr;
+            new_instr.key = key_to_hash(c);
+            new_instr.repeat = 1;
+            temp_instruction_set.push_back(new_instr);
+        }
+
+        // Still pattern match on the raw string for regex
+        // But write to the temp_instruction vector
+        pattern_match(raw_instructions);
+
+        // Goes through temp instr vector, adds to counter if duplicates found
+        // in a row
+        std::cout << "YOYO";
+        coalesce_operands();
+
+        std::cout << "YOYO";
+        //create_final_instr();
 
         matching_loop();
     }
 
     // Takes in command string, iterates through it moving commands into vector
     // Handles condensing duplicate operations (e.g. +++), and patter matching, e.g. "[-]"
-    void coalesce_operands(std::string instructions) {
-        const std::string condensed_chars = "+-><";
+    void coalesce_operands() {
+        const std::vector<instructions> CONDENSABLE_INSTRS = { traverse_right, traverse_left, increment, decrement };
 
-        instructions = pattern_match(instructions);
+        for (int i = 0; i < temp_instruction_set.size(); i++) {
 
-        // Push initial instruction
-        Cell new_cell = { instructions[0], 1 };
-        final_instruction_set.push_back(new_cell);
-
-        for (int i = 1; i < instructions.size(); i++) {
-            // Skip if deleted by pattern matcher
-            if (instructions[i] == '0') {
+            // If it matches former key, add to vec.tops repeat and don't add this new one
+            // to final instr vector
+            if (final_instruction_set.size() > 0
+                && final_instruction_set.back().key == temp_instruction_set[i].key
+                && std::find(CONDENSABLE_INSTRS.begin(), CONDENSABLE_INSTRS.end(), temp_instruction_set[i].key) != CONDENSABLE_INSTRS.end()) {
+                final_instruction_set.back().repeat++;
                 continue;
             }
 
-            // Condense instr into former instr if match + allowed char
-            if (final_instruction_set.back().instruction == instructions[i]
-                && condensed_chars.find(instructions[i]) != std::string::npos) {
-                final_instruction_set.back().counter++;
-            }
-            else {
-                // Add new instruction
-                // Counter defaults to 1, brackets will overwrite this
-                Cell new_cell = { instructions[i], 1 };
-                final_instruction_set.push_back(new_cell);
-            }
+            // Add new instruction to final instr vec
+            final_instruction_set.push_back(temp_instruction_set[i]);
         }
     }
 
@@ -186,13 +230,13 @@ public:
     // Takes in string of raw instructions, searches for common patterns
     // Replaces the first instr of that pattern with a unique key and replaces
     // the rest of the pattern with a 0
-    std::string pattern_match(std::string instructions) {
+    void pattern_match(std::string raw_instructions) {
         // Sometimes the instr will save data into the next removed char instead of 0
-        std::vector<std::pair<std::string, char>> patterns = { 
-            {"\\[-\\]", 'z'},
+        std::vector<std::pair<std::string, instructions>> patterns = { 
+            {"\\[-\\]", pattern_zero},
             //Adds/moves
-            {"\\[->+\\+<+\\]", 't'}, //Simple trade (any number of < or >), must check < and > match for each match
-            {"\\[>+\\+<+-\\]", 't'},
+            {"\\[->+\\+<+\\]", pattern_move}, //Simple trade (any number of < or >), must check < and > match for each match
+            {"\\[>+\\+<+-\\]", pattern_move},
             //Copy
             //Needs aux + destination cells
 
@@ -207,35 +251,30 @@ public:
             // Number of < and > are balanced
             // [<->-<<<<<<+>>>>>>] (big on left of small, answer to left)
             // [>-<->>>>>>+<<<<<<] (big on right of small, answer to right)
-            {"\\[<+->+-<+\\+>+\\]", 's'},
-            {"\\[   >+-<+   <+\\+>+        \\]", '1'},
-            {"\\[   <+->+   >+\\+<+        \\]", '2'},
-            {"\\[   >+-<+   >+\\+<+        \\]", '3'},
+            {"\\[<+->+-<+\\+>+\\]", pattern_substract},
+            {"\\[   >+-<+   <+\\+>+        \\]", pattern_substract},
+            {"\\[   <+->+   >+\\+<+        \\]", pattern_substract},
+            {"\\[   >+-<+   >+\\+<+        \\]", pattern_substract},
         };
 
         std::vector<int> index_matches;
         std::regex rx;
-        int extra_byte;
-        int extra_byte2;
+        //std::vector<int> extra_bytes;
 
         for (auto pattern : patterns) {
             rx = pattern.first;
             index_matches.clear();
 
-            auto words_begin = std::sregex_iterator(instructions.begin(), instructions.end(), rx);
+            auto words_begin = std::sregex_iterator(raw_instructions.begin(), raw_instructions.end(), rx);
             auto words_end = std::sregex_iterator();
 
             for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
                 std::smatch match = *i;
                 std::string match_str = match.str();
 
-                // Set this if need an extra byte of data
-                extra_byte = 0;
-                extra_byte2 = 0;
-
                 // If it's an s, check for matching < and >
                 // [<-> - <<<<<<+>>>>>>]
-                if (pattern.second == 's') {
+                if (pattern.second == pattern_substract) {
                     // Split the match into 2 substrs, before and after the 2nd '-'
                     // Check for balanced <> in each one
                     size_t pos = match_str.find("-");
@@ -243,50 +282,56 @@ public:
 
                     std::string half_one = match_str.substr(0, pos);
                     std::string half_two = match_str.substr(pos, std::string::npos);
-                    //std::cout << match_str << " : " << match_str.substr(0, pos) << std::endl;
-                    //std::cout << match_str << " : " << match_str.substr(pos, std::string::npos) << std::endl;
                     
                     //Check halves
-                    // Store this number after 's' to indicate how many to move
-                    extra_byte = check_balance_traverse(half_one);
-                    extra_byte2 = check_balance_traverse(half_two);
+                    int dist_to_big = check_balance_traverse(half_one);
+                    int dist_to_ans = check_balance_traverse(half_two);
 
                     // Returns 0 if not balanced, don't overwrite anything
-                    if (extra_byte == 0 || extra_byte2 == 0) continue;
+                    if (dist_to_big == 0 || dist_to_ans == 0) continue;
+
+                    // Write the distances to the instr in temp vector
+                    temp_instruction_set[i->position()].dist_to_big = dist_to_big;
+                    temp_instruction_set[i->position()].dist_to_ans = dist_to_ans;
                 }
 
                 // If it's a 't' (trade), check for matching < and >
-                if (pattern.second == 't') {
-                    auto next_count = std::count(match_str.begin(), match_str.end(), '>');
-                    auto prev_count = std::count(match_str.begin(), match_str.end(), '<');
+                if (pattern.second == pattern_move) {
+                    int dist = check_balance_traverse(match_str);
 
-                    // If not, don't replace anything
-                    if (next_count != prev_count) {
-                        continue;
-                    }
+                    if (dist == 0) continue;
 
-                    // Store this number after 't' to indicate how many spaces away the trade is
-                    extra_byte = next_count;
+                    temp_instruction_set[i->position()].dist = dist;
                 }
 
-                // Replace the word with key and 0s
-                instructions[i->position()] = pattern.second;
+                // Replace the key of the first instruction in the temp vector
+                // With the key defined in pair above
+                temp_instruction_set[i->position()].key = pattern.second;
+
+                // For all other instructions in the pattern, replace key with '0'
                 for (int j = 1; j < match_str.size(); j++) {
-                    instructions[i->position() + j] = '0';
+                    temp_instruction_set[i->position() + j].key = dead_instr;
                 }
 
-                if (extra_byte != 0) {
-                    instructions[i->position() + 1] = extra_byte;
-                }
 
-                if (extra_byte2 != 0) {
-                    instructions[i->position() + 2] = extra_byte2;
-                }
+
+                //// Replace the word with key and 0s
+                //instructions[i->position()] = pattern.second;
+                //for (int j = 1; j < match_str.size(); j++) {
+                //    instructions[i->position() + j] = '0';
+                //}
+
+                //// Write extra bytes if needed
+                //for (int j = 0; j < extra_bytes.size(); j++) {
+                //    if (extra_bytes[j] != 0) {
+                //        instructions[i->position() + j + 1] = extra_bytes[j];
+                //    }
+                //}
             }
         }
 
-        //std::cout << instructions << std::endl;
-        return instructions;
+        std::cout << raw_instructions << std::endl;
+        //return instructions;
     }
 
     // Checks for valid character set
@@ -310,11 +355,11 @@ public:
         for (int i = 0; i < final_instruction_set.size(); i++) {
             // Store its own index in its pair
             // When closing is found, swap their indices
-            if (final_instruction_set[i].instruction == '[') {
+            if (final_instruction_set[i].key == loop_open) {
                 st.push({ '[', i });
             }
 
-            if (final_instruction_set[i].instruction == ']') {
+            if (final_instruction_set[i].key == loop_close) {
                 // Bad code check
                 if (st.size() == 0) {
                     std::cout << "Error: Missing opening bracket.";
@@ -325,8 +370,8 @@ public:
                 int closer_index = i;
 
                 // Swap to correct moveto indices
-                final_instruction_set[closer_index].counter = opener_index + 1;
-                final_instruction_set[opener_index].counter = closer_index + 1;
+                final_instruction_set[closer_index].moveto = opener_index + 1;
+                final_instruction_set[opener_index].moveto = closer_index + 1;
 
                 // Remove the pair
                 st.pop();
@@ -340,8 +385,8 @@ public:
 
         if (debug) {
             std::cout << "Final instruction set: (instruction:counter/moveto index)\n";
-            for (Cell c : final_instruction_set) {
-                std::cout << c.instruction << ":" << c.counter << std::endl;
+            for (Instruction c : final_instruction_set) {
+                std::cout << c.key << ":" << c.repeat << std::endl;
             }
             std::cout << std::endl;
         }
@@ -350,13 +395,13 @@ public:
     void execute() {
         long long int instr_counter = 0;
 
-        Cell command;
+        Instruction instr;
         for (curr_instruction_index; curr_instruction_index < final_instruction_set.size(); curr_instruction_index++) {
-            command = final_instruction_set[curr_instruction_index];
+            instr = final_instruction_set[curr_instruction_index];
             //std::cout << command.instruction << "\n";
 
-            switch (command.instruction) {
-            case 's': {
+            switch (instr.key) {
+            case pattern_substract: {
                 if (tape[curr_tape_index] == 0) break;
 
                 // ONLY HANDLES DEFAULT SCENARIO
@@ -364,76 +409,76 @@ public:
                 tape[curr_tape_index] = 0;
 
                 // Num stored in instruction
-                int move_to_big = final_instruction_set[curr_instruction_index + 1].instruction;
+                int move_to_big = final_instruction_set[curr_instruction_index].dist_to_big;
 
-                int big_val = tape[curr_tape_index - +move_to_big];
-                tape[curr_tape_index - +move_to_big] -= +small_val;
+                int big_val = tape[curr_tape_index - move_to_big];
+                tape[curr_tape_index - move_to_big] -= small_val;
 
-                int answer_val = +big_val - +small_val;
+                int answer_val = big_val - small_val;
                 if (answer_val == 0) answer_val = 1; // Always moves 1 minimum
 
-                int move_to_answer = final_instruction_set[curr_instruction_index + 2].instruction;
+                int move_to_answer = final_instruction_set[curr_instruction_index].dist_to_ans;
                 // Adds to the cell, not replace
-                tape[curr_tape_index - +move_to_answer] += +answer_val;
+                tape[curr_tape_index - move_to_answer] += answer_val;
 
                 break;
             }
-            case 't': {
+            case pattern_move: {
                 // This takes number of current loc, sets to 0, and adds to the pointed loc
                 // pointed loc is bytes to the right specified by the byte after 't'
                 int val = tape[curr_tape_index];
-                int moves = final_instruction_set[curr_instruction_index+1].instruction;
+                int moves = final_instruction_set[curr_instruction_index].dist;
 
                 tape[curr_tape_index] = 0;
                 tape[curr_tape_index + +moves] += val;
 
                 break;
             }
-            case 'z':
+            case pattern_zero:
                 tape[curr_tape_index] = 0;
                 break;
-            case '>':
+            case traverse_right:
                 if (do_fast) {
-                    curr_tape_index += command.counter;
+                    curr_tape_index += instr.repeat;
                 }
                 else {
-                    traverse_next(command.counter);
+                    traverse_next(instr.repeat);
                 }
                 break;
-            case '<':
+            case traverse_left:
                 if (do_fast) {
-                    curr_tape_index -= command.counter;
+                    curr_tape_index -= instr.repeat;
                 }
                 else {
-                    traverse_prev(command.counter);
+                    traverse_prev(instr.repeat);
                 }
                 break;
-            case '+':
+            case increment:
                 if (do_fast) {
-                    tape[curr_tape_index] += command.counter;
+                    tape[curr_tape_index] += instr.repeat;
                 }
                 else {
-                    increment_data(command.counter);
+                    increment_data(instr.repeat);
                 }
                 break;
-            case '-':
+            case decrement:
                 if (do_fast) {
-                    tape[curr_tape_index] -= command.counter;
+                    tape[curr_tape_index] -= instr.repeat;
                 }
                 else {
-                    decrement_data(command.counter);
+                    decrement_data(instr.repeat);
                 }
                 break;
-            case ',':
+            case input:
                 getchar();
                 break;
-            case '[':
-                loop_open();
+            case loop_open:
+                floop_open();
                 break;
-            case ']':
-                loop_close();
+            case loop_close:
+                floop_close();
                 break;
-            case '.':
+            case output:
                 output_byte();
                 break;
             default:
